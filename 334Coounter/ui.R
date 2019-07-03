418b5e32c61dd897472ef38324a530ecb3043a90
Syouki Tsuyama <s-tsuyama@economic-index.co.jp>
2019-07-03 17:18:19 +0900

    po

---
+++ 334Coounter/ui.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+shinyUI(
+    fluidPage(
+
+        titlePanel(
+            h1("334-counter")
+        ),
+
+        sidebarPanel(
+            h3("33-4")
+            actionButton("action", "Action")
+        )
+
+    )
+)
