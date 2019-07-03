b7fecf11baddb8d1b3d167be56d3a595aa2bb23d
Syouki Tsuyama <s-tsuyama@economic-index.co.jp>
2019-07-03 18:04:28 +0900

    po

--- 334Counter/server.R
+++ 334Counter/server.R
@@ -5,8 +5,8 @@ count2 = 0
 
 shinyServer(function(input, output) {
     output$count <- renderUI({
-        count1 += 33
-        count2 += 4
+        count1 <- count1 + 33
+        count2 <- count2 + 4
 
         h1(paste(count1, " - ", count2))
     })
