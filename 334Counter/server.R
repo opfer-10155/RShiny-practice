8cc338a641f19667a2891cb0e427ea66506bef58
Syouki Tsuyama <s-tsuyama@economic-index.co.jp>
2019-07-05 16:29:58 +0900

    po

--- 334Counter/server.R
+++ 334Counter/server.R
@@ -5,9 +5,8 @@ count2 <- 4
 
 shinyServer(function(input, output) {
 
-    # イベントハンドラではないらしい
+    # イベントハンドラではないらしい、inputをReactのstateのように、変化するたびに再レンダリング？
     output$count <- renderUI({
-
         h1(paste(count1*input$action, " - ", count2*input$action))
     })
 })
---
+++ census/server.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(maps)
+library(mapproj)
+
+source("helpers.R")
+
+counties <- readRDS("data/counties.rds")
+
+shinyServer(function(input, output) {
+    output$map <- renderPlot({
+        data <- switch(
+            input$var,
+            "Percent White"   = counties$white,
+            "Percent Black"   = counties$black,
+            "Percent Hispanic"= counties$hispanic,
+            "Percent Asian"   = counties$asian
+        )
+
+        title <- switch(
+            input$var,
+            "Percent White"    = "% White"
+            "Percent Black"    = "% Black"
+            "Percent Hispanic" = "% Hispanic"
+            "Percent Hispanic" = "% Hispanic"
+        )
+
+        percent_map(
+            data,
+            color="darkgreen",
+            legend.title=title,
+            max=input$range[0]
+            min=input$range[1]
+        )
+    })
+})
