d7fc9bf3c9128db167e0788588333ce60d757acc
Syouki Tsuyama <s-tsuyama@economic-index.co.jp>
2019-07-05 15:03:29 +0900

    po

--- 334Counter/server.R
+++ 334Counter/server.R
@@ -1,16 +1,13 @@
 library(shiny)
 
-count1 <- 0
-count2 <- 0
+count1 <- 33
+count2 <- 4
 
 shinyServer(function(input, output) {
 
     # イベントハンドラではないらしい
     output$count <- renderUI({
-        count1 <- count1 + 33
-        count2 <- count2 + 4
 
-        h1(input$action)
+        h1(paste(count1*input$action, " - ", count2*input$action))
     })
 })
-# paste(count1, " - ", count2)
